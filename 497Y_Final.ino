2514099d39a4c902041fbe6154f43801c5714d22
Bryce285 <bryceclark237@gmail.com>
2026-02-18 17:20:36 -0800

    initial commit

---
+++ 497Y_Final.ino
@@ -0,0 +1,201 @@
+/*
+    TODO - DOUBLE CHECK THAT THESE PIN ASSIGNMENTS WORK WITH CIRCUIT LAYOUT BEFORE CONSTRUCTION
+*/
+const int SWITCH = 10;
+const int BUTTON = A9;
+const int LIGHT_SENSOR = A8;
+const int BUZZER = A7;
+
+/*
+    LEDs are labeled according to the bit significance of their position
+    on the garment.
+    LED0 is in the place of the least significant bit while LED5
+    is in the place of the most significant bit.
+*/
+const int LED0 = A4;
+const int LED1 = A5;
+const int LED2 = 6;
+const int LED3 = A3;
+const int LED4 = A2;
+
+void setup() 
+{
+  pinMode(SWITCH, INPUT);
+  digitalWrite(SWITCH, HIGH);
+
+  pinMode(BUTTON, INPUT);
+  digitalWrite(BUTTON, HIGH);
+
+  pinMode(LIGHT_SENSOR, INPUT);
+  
+  pinMode(BUZZER, OUTPUT);
+  pinMode(LED0, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+}
+
+enum class AlertMode 
+{
+  BUZZ,         // alert with buzzer only
+  LIGHT,        // alert with LEDs only
+  ALL,          // alert with both buzzer and LEDs
+  VARIABLE      // alert based on current light level
+};
+
+struct Timer
+{
+  AlertMode cur_mode = AlertMode::ALL;
+  bool active = false;
+  bool done = false;
+
+  unsigned long time_remaining = 0;
+  unsigned long last_tick = 0;
+
+  AlertMode next_mode(AlertMode mode)
+  {
+    switch (mode)
+    {
+      case AlertMode::BUZZ: return AlertMode::LIGHT;
+      case AlertMode::LIGHT: return AlertMode::ALL;
+      case AlertMode::ALL: return AlertMode::VARIABLE;
+      case AlertMode::VARIABLE return AlertMode::BUZZ;
+    }
+
+    return AlertMode::ALL;
+  }
+
+  /*
+      This function essentially stores the time remaining for the timer in seconds
+  */
+  void set(unsigned long start_val_hrs)
+  {
+    time_remaining = start_val_hrs * 60UL * 60UL;
+  }
+
+  /*
+      A tick is approximately one second 
+      It's not exact but it's close enough for this project
+  */
+  bool decrement()
+  {
+    if (millis() - last_tick >= 1000) {
+      last_tick = millis();
+      time_remaining--;
+
+      if (time_remaining == 0) {
+        done = true;
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  unsigned long to_hours()
+  {
+    if (time_remaining < 3600) return 0;
+    return (time_remaining / 60UL) / 60UL;
+  }
+};
+
+struct Button
+{
+  unsigned long press_start = 0;
+  unsigned long cur_press_duration = 0;
+  unsigned long final_press_duration = 0;
+  bool was_pressed = false;
+
+  void reset()
+  {
+    press_start = 0;
+    cur_press_duration = 0;
+    final_press_duration = 0;
+    was_pressed = false;
+  }
+};
+
+struct LEDs
+{
+  void write_value(unsigned int value)
+  {
+    if (value > 24) {
+      digitalWrite(LED0, LOW);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
+      return;
+    }
+
+    unsigned int ones_bit = value & 1U;
+    unsigned int twos_bit = (value & (1U << 1)) >> 1;
+    unsigned int threes_bit = (value & (1U << 2)) >> 2;
+    unsigned int fours_bit = (value & (1U << 3)) >> 3;
+    unsigned int fives_bit = (value & (1U << 4)) >> 4;
+
+    if (ones_bit == 1) digitalWrite(LED0, HIGH);
+    else digitalWrite(LED0, LOW);
+
+    if (twos_bit == 1) digitalWrite(LED1, HIGH);
+    else digitalWrite(LED1, LOW);
+
+    if (threes_bit == 1) digitalWrite(LED2, HIGH);
+    else digitalWrite(LED2, LOW);
+
+    if (fours_bit == 1) digitalWrite(LED3, HIGH);
+    else digitalWrite(LED3, LOW);
+
+    if (fives_bit == 1) digitalWrite(LED4, HIGH);
+    else digitalWrite(LED4, LOW);
+  }
+};
+
+Timer timer;
+Button button;
+LEDs leds;
+
+void loop()
+{
+  if (timer.active && timer.decrement() == true) {
+    // TODO - alert based on the selected mode
+  }
+
+  leds.write_value(timer.to_hours());
+
+  /*
+      Holding the button down increments the starting timer value each second up to a
+      starting value of 24 hours.
+      Pressing the button for less than 1 second cycles through the alert modes.
+  */
+  if ((digitalRead(BUTTON) == LOW) && button.was_pressed) {
+    button.cur_press_duration = millis() - button.press_start;
+
+    // TODO - check cur press duration and light up LEDs accordingly to show
+    // the starting timer value
+  }
+  else if ((digitalRead(BUTTON) == LOW) && !button.was_pressed) {
+    button.press_start = millis();
+    button.was_pressed = true;
+  }
+  else if ((digitalRead(BUTTON) == HIGH) && button.was_pressed) {
+    button.final_press_duration = millis() - button.press_start;
+    button.was_pressed = false;
+
+    if (button.final_press_duration < 1000) {
+      timer.cur_mode = timer.next_mode(timer.cur_mode);
+    }
+
+    button.reset();
+  }
+
+  // the switch lets users cancel the current timer
+  if (digitalRead(SWITCH) == LOW) {
+    timer.active = false;
+
+    // TODO - add a reset function for the timer similar to what is in the button struct and
+    // call it here
+  }
+
+}
