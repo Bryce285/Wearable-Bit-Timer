f15f8fec8df78ea2a1931ebc7425be56bb157dcd
Bryce285 <bryceclark237@gmail.com>
2026-03-12 12:35:34 -0700

    program fully functional

--- 497Y_Final/497Y_Final.ino
+++ 497Y_Final/497Y_Final.ino
@@ -43,6 +43,42 @@ enum class AlertMode
   VARIABLE      // alert based on current light level
 };
 
+namespace LEDs
+{
+  void write_value(unsigned int value)
+  {
+    if (value > 31) {
+      digitalWrite(LED0, LOW);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
+      return;
+    }
+
+    unsigned int ones_bit = value & 1U;
+    unsigned int twos_bit = (value & (1U << 1)) >> 1;
+    unsigned int fours_bit = (value & (1U << 2)) >> 2;
+    unsigned int eights_bit = (value & (1U << 3)) >> 3;
+    unsigned int sixteens_bit = (value & (1U << 4)) >> 4;
+
+    if (ones_bit == 1) digitalWrite(LED0, HIGH);
+    else digitalWrite(LED0, LOW);
+
+    if (twos_bit == 1) digitalWrite(LED1, HIGH);
+    else digitalWrite(LED1, LOW);
+
+    if (fours_bit == 1) digitalWrite(LED2, HIGH);
+    else digitalWrite(LED2, LOW);
+
+    if (eights_bit == 1) digitalWrite(LED3, HIGH);
+    else digitalWrite(LED3, LOW);
+
+    if (sixteens_bit == 1) digitalWrite(LED4, HIGH);
+    else digitalWrite(LED4, LOW);
+  }
+};
+
 namespace Timer
 {
   AlertMode cur_mode = AlertMode::ALL;
@@ -55,15 +91,27 @@ namespace Timer
   unsigned long time_remaining = 0;
   unsigned long last_tick = 0;
 
+  int light_read = -1;
+
   AlertMode next_mode(AlertMode mode)
   {
     switch (mode)
     {
-      case AlertMode::BUZZ: return AlertMode::LIGHT;
-      case AlertMode::LIGHT: return AlertMode::ALL;
-      case AlertMode::ALL: return AlertMode::VARIABLE;
-      case AlertMode::VARIABLE: return AlertMode::BUZZ;
-      default: return AlertMode::ALL;
+      case AlertMode::BUZZ: 
+        Serial.println("Light mode set");
+        return AlertMode::LIGHT;
+      case AlertMode::LIGHT:
+        Serial.println("All mode set"); 
+        return AlertMode::ALL;
+      case AlertMode::ALL:
+        Serial.println("Variable mode set"); 
+        return AlertMode::VARIABLE;
+      case AlertMode::VARIABLE:
+        Serial.println("Buzz mode set"); 
+        return AlertMode::BUZZ;
+      default:
+        Serial.println("All mode set"); 
+        return AlertMode::ALL;
     }
 
     return AlertMode::ALL;
@@ -107,15 +155,34 @@ namespace Timer
     return (time_remaining / 60UL);
   }
 
+  void reset()
+  {
+    cur_mode = AlertMode::ALL;
+    active = false;
+    done = false;
+    input_received = false;
+    time_remaining = 0;
+    last_tick = 0;
+    light_read = -1;
+  }
+
   void buzz_mode()
   {
     if (input_received) {
-      digitalWrite(BUZZER, LOW);
-      active = false;
+      noTone(BUZZER);
+      reset();
       return;
     }
 
-    digitalWrite(BUZZER, HIGH);
+    if (cur_mode != AlertMode::ALL && (cur_mode != AlertMode::VARIABLE && analogRead(LIGHT_SENSOR) > 20)) {
+      digitalWrite(LED0, LOW);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
+    }
+
+    tone(BUZZER, 440);
   }
 
   /* This implementation makes the lights blink without blocking user input */
@@ -127,22 +194,19 @@ namespace Timer
       digitalWrite(LED2, LOW);
       digitalWrite(LED3, LOW);
       digitalWrite(LED4, LOW);
-      active = false;
+      reset();
       return;
     }
 
-    static unsigned long last_blink = 0;
-    static bool state = false;
+    if (cur_mode != AlertMode::ALL) noTone(BUZZER);
 
-    if (millis() - last_blink >= 250) {
-      last_blink = millis();
-      state = !state;
+    static unsigned long last_step = 0;
+    static uint8_t pos = 0;
 
-      digitalWrite(LED0, state);
-      digitalWrite(LED1, state);
-      digitalWrite(LED2, state);
-      digitalWrite(LED3, state);
-      digitalWrite(LED4, state);
+    if (millis() - last_step >= 500) {
+      last_step = millis();
+      LEDs::write_value((1 << (pos + 1)) - 1);
+      pos = (pos + 1) % 5;
     }
   }
 
@@ -154,7 +218,11 @@ namespace Timer
 
   void variable_mode()
   {
-    if (analogRead(LIGHT_SENSOR) < 20) {
+    if (light_read < 0) {
+      light_read = analogRead(LIGHT_SENSOR);
+    }
+
+    if (light_read < 20) {
       light_mode();
     }
     else {
@@ -178,72 +246,36 @@ namespace Timer
       variable_mode();
     }
   }
-
-  void reset()
-  {
-    cur_mode = AlertMode::ALL;
-    active = false;
-    done = false;
-    input_received = false;
-    time_remaining = 0;
-    last_tick = 0;
-  }
 };
 
 namespace Button
 {
   unsigned long press_start = 0;
-  unsigned long cur_press_duration = 0;
-  unsigned long final_press_duration = 0;
+  unsigned long last_change_time = 0;
+  bool last_state = HIGH;
+  bool stable_state = HIGH;
   bool was_pressed = false;
 
-  void reset()
-  {
-    press_start = 0;
-    cur_press_duration = 0;
-    final_press_duration = 0;
-    was_pressed = false;
-  }
+  const unsigned long DEBOUNCE_MS = 50;
 };
 
-namespace LEDs
-{
-  void write_value(unsigned int value)
-  {
-    if (value > 31) {
-      digitalWrite(LED0, LOW);
-      digitalWrite(LED1, LOW);
-      digitalWrite(LED2, LOW);
-      digitalWrite(LED3, LOW);
-      digitalWrite(LED4, LOW);
-      return;
-    }
+unsigned long last_read = millis();
+int light;
 
-    unsigned int ones_bit = value & 1U;
-    unsigned int twos_bit = (value & (1U << 1)) >> 1;
-    unsigned int fours_bit = (value & (1U << 2)) >> 2;
-    unsigned int eights_bit = (value & (1U << 3)) >> 3;
-    unsigned int sixteens_bit = (value & (1U << 4)) >> 4;
-
-    if (ones_bit == 1) digitalWrite(LED0, HIGH);
-    else digitalWrite(LED0, LOW);
-
-    if (twos_bit == 1) digitalWrite(LED1, HIGH);
-    else digitalWrite(LED1, LOW);
-
-    if (fours_bit == 1) digitalWrite(LED2, HIGH);
-    else digitalWrite(LED2, LOW);
-
-    if (eights_bit == 1) digitalWrite(LED3, HIGH);
-    else digitalWrite(LED3, LOW);
-
-    if (sixteens_bit == 1) digitalWrite(LED4, HIGH);
-    else digitalWrite(LED4, LOW);
-  }
-};
+int raw_button;
+unsigned long now;
+unsigned long press_duration;
+unsigned long hold_duration;
 
 void loop()
 {
+  if (millis() - last_read > 5000) {
+    light = analogRead(LIGHT_SENSOR);
+    Serial.print("Light sensor reading: ");
+    Serial.println(light);
+    last_read = millis();
+  }
+
   if (Timer::active) {
     if (Timer::decrement()) {
       Timer::done = true;
@@ -254,52 +286,57 @@ void loop()
     }
   }
 
-  LEDs::write_value(Timer::to_mins());
+  Timer::input_received = false;
 
-  /*
-      Holding the button down increments the starting timer value each second up to a
-      starting value of 31 minutes.
-      Pressing the button for less than 1 second cycles through the alert modes.
-  */
-  if ((digitalRead(BUTTON) == LOW) && Button::was_pressed) {
-    Serial.println("Button pressed");
-    Button::cur_press_duration = millis() - Button::press_start;
+  if (!Timer::done) {
+    LEDs::write_value(Timer::to_mins());
+  }
 
-    /*check current press duration and light up LEDs accordingly to show
-    the starting timer value */
-    if (Button::cur_press_duration > 1000) {
-      Timer::selected_mins = Button::cur_press_duration / 1000;
+  raw_button  = digitalRead(BUTTON);
+  now = millis();
+
+  if (raw_button != Button::last_state) {
+    Button::last_change_time = now;
+    Button::last_state = raw_button;
+  }
+
+  if (now - Button::last_change_time > Button::DEBOUNCE_MS) {
+    if (Button::stable_state != raw_button) {
+      Button::stable_state = raw_button;
+
+      if (Button::stable_state == LOW) {
+        Button::press_start = now;
+        Button::was_pressed = true;
+        Timer::input_received = true;
+      }
+
+      else if (Button::stable_state == HIGH && Button::was_pressed) {
+        press_duration = now - Button::press_start;
+        Button::was_pressed = false;
+
+        if (press_duration < 1000) {
+          Timer::cur_mode = Timer::next_mode(Timer::cur_mode);
+        }
+        else {
+          Timer::set(Timer::selected_mins);
+          Timer::active = true;
+          Timer::done = false;
+        }
+      }
+    }
+  }
+
+  if (Button::was_pressed && Button::stable_state == LOW) {
+    hold_duration = now - Button::press_start;
+    if (hold_duration > 1000) {
+      Timer::selected_mins = hold_duration / 1000;
       if (Timer::selected_mins > 31) Timer::selected_mins = 31;
-
       LEDs::write_value(Timer::selected_mins);
     }
   }
-  else if ((digitalRead(BUTTON) == LOW) && !Button::was_pressed) {
-    Serial.println("Button pressed");
-    Button::press_start = millis();
-    Button::was_pressed = true;
-    Timer::input_received = true;
-  }
-  else if ((digitalRead(BUTTON) == HIGH) && Button::was_pressed) {
-    Button::final_press_duration = millis() - Button::press_start;
-    Button::was_pressed = false;
 
-    if (Button::final_press_duration < 1000) {
-      Timer::cur_mode = Timer::next_mode(Timer::cur_mode);
-    }
-    else if (Button::final_press_duration > 1000) {
-      Timer::active = true;
-      Timer::done = false;
-      Timer::set(Timer::selected_mins);
-    }
-
-    Button::reset();
-    Timer::input_received = false;
-  }
-
-  /* the switch lets users cancel the current timer */
+  
   if (digitalRead(SWITCH) == LOW) {
-    Serial.println("Switch flipped");
     Timer::reset();
   }
 }
